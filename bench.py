c9f945e4810ec19bf863c4ea0bc6820433497423
Jon Morton <jon@jamorton.com>
2023-11-13 21:14:22 -0800

    Improve formatting

--- bench.py
+++ bench.py
@@ -17,11 +17,15 @@ def run_bench(args, bench: Bench):
         for _ in range(args.iters):
             t = time.perf_counter()
             bench.run()
-            timings.append(time.perf_counter() - t)
+            timings.append((time.perf_counter() - t) * 1000)
 
         time.sleep(1.0)
 
-    return statistics.mean(timings), statistics.stdev(timings)
+    return (
+        statistics.mean(timings),
+        statistics.stdev(timings),
+        statistics.quantiles(sorted(timings), n=10)[-1],
+    )
 
 
 def get_pytorch_bench(name):
@@ -40,40 +44,34 @@ def get_all_benches():
     from jax_benches import JAX_BENCHES
     from pytorch_benches import PYTORCH_BENCHES
 
-    return list(set(JAX_BENCHES.keys()).intersection(set(PYTORCH_BENCHES.keys())))
+    return sorted(set(JAX_BENCHES.keys()).intersection(set(PYTORCH_BENCHES.keys())))
 
 
 def run(bench_name):
-    ptmean, ptstd = run_bench(args, get_pytorch_bench(bench_name))
+    ptmean, ptstd, p_p90 = run_bench(args, get_pytorch_bench(bench_name))
     time.sleep(2.0)
-    jaxmean, jaxstd = run_bench(args, get_jax_bench(bench_name))
+    jaxmean, jaxstd, j_p90 = run_bench(args, get_jax_bench(bench_name))
 
     print(bench_name)
-    print(f"  pytorch: {ptmean * 1000:.1f}ms±{ptstd * 1000:.2f}ms")
-    print(f"  jax: {jaxmean * 1000:.1f}ms±{jaxstd * 1000:.2f}ms")
+    print("-" * len(bench_name))
+    print(f"pytorch:{ptmean:5.1f}ms ±{ptstd:5.2f}ms p90={p_p90:.1f}ms ")
+    print(f"jax:    {jaxmean:5.1f}ms ±{jaxstd:5.2f}ms p90={j_p90:.1f}ms")
+    print("")
 
 
 def main(args):
-    import torch
+    all_benches = get_all_benches()
 
-    # jax uses tf32 by default.
-    torch.set_float32_matmul_precision("high")
+    if args.benches == "all":
+        benches = all_benches
+    else:
+        benches = args.benches.split(",")
 
-    import jax
-
-    with jax.default_matmul_precision("tensorfloat32"):
-        all_benches = get_all_benches()
-
-        if args.benches == "all":
-            benches = all_benches
-        else:
-            benches = args.benches.split(",")
-
-        for b in benches:
-            if b not in all_benches:
-                raise Exception(f"Invalid bench name '{b}'")
-            time.sleep(2.0)
-            run(b)
+    for b in benches:
+        if b not in all_benches:
+            raise Exception(f"Invalid bench name '{b}'")
+        time.sleep(2.0)
+        run(b)
 
 
 def get_argument_parser():
